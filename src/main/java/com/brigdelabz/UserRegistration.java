72a71ccb010a459f75ac6be58a2040bdaf650f7d
SAHIL0455 <sahilpawar1004@gmail.com>
2022-07-01 19:28:22 +0530

    ADDED: write junit test to validate the User Entry

--- src/main/java/com/brigdelabz/UserRegistration.java
+++ src/main/java/com/brigdelabz/UserRegistration.java
@@ -3,6 +3,31 @@ package com.brigdelabz;
 import java.util.regex.Pattern;
 
 public class UserRegistration<T> {
+    public boolean validateFirstName(T firstName) {
+        boolean result = (Pattern.matches("[a-zA-z\\d]{3,10}", (CharSequence) firstName));
+        return result;
+    }
+
+    public boolean validateLastName(T lastName) {
+        boolean result = (Pattern.matches("[a-zA-z\\d]{3,10}", (CharSequence) lastName));
+        return result;
+    }
+
+    public boolean validateEmail(T emailID) {
+        boolean result = (Pattern.matches("[^.][a-zA-Z\\d+_.-]{2,}[@][a-zA-Z]{2,}[.][a-z.]{2,}", (CharSequence) emailID));
+        return result;
+    }
+
+    public boolean validateMobile(T mobile) {
+        boolean result = (Pattern.matches("[789]{1}[\\d]{9}", (CharSequence) mobile));
+        return result;
+    }
+
+    public boolean validatePassword(T password) {
+        boolean result = Pattern.matches("(?=.*?[A-Z])(?=.*?[!@#$^&*.])(?=.*?[0-9])([a-zA-Z\\d@*#$&,.]{8,})", (CharSequence) password);
+        return result;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to junit");
     }
--- src/test/java/com/brigdelabz/UserRegistrationTest.java
+++ src/test/java/com/brigdelabz/UserRegistrationTest.java
@@ -5,4 +5,44 @@ import org.junit.Test;
 
 public class UserRegistrationTest {
     static UserRegistration userRegistration = new UserRegistration();
+
+    @Test
+    public void validateFirstNameTest() {
+        boolean actual1 = userRegistration.validateFirstName("Rohit");
+        boolean actual2 = userRegistration.validateFirstName("Ro-hit");
+        Assert.assertTrue(actual1);
+        Assert.assertFalse(actual2);
+    }
+
+    @Test
+    public void validateLastNameTest() {
+        boolean actual1 = userRegistration.validateLastName("Sharma");
+        boolean actual2 = userRegistration.validateLastName("Sharma.");
+        Assert.assertTrue(actual1);
+        Assert.assertFalse(actual2);
+    }
+
+    @Test
+    public void validateEmailTest() {
+        boolean actual1 = userRegistration.validateEmail("abc-100@yahoo.com");
+        boolean actual2 = userRegistration.validateEmail(".rohit@gmail.com");
+        Assert.assertTrue(actual1);
+        Assert.assertFalse(actual2);
+    }
+
+    @Test
+    public void validateMobile() {
+        boolean actual1 = userRegistration.validateMobile("7350310455");
+        boolean actual2 = userRegistration.validateMobile("6395824158");
+        Assert.assertTrue(actual1);
+        Assert.assertFalse(actual2);
+    }
+
+    @Test
+    public void validatePassword() {
+        boolean actual1 = userRegistration.validatePassword("Sahil@1233");
+        boolean actual2 = userRegistration.validatePassword("sahil12");
+        Assert.assertFalse(actual2);
+        Assert.assertTrue(actual1);
+    }
 }
