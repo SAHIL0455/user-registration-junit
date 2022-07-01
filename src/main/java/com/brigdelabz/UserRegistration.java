f6dc6267b24556d06465449dd32ba292750b2e86
SAHIL0455 <sahilpawar1004@gmail.com>
2022-07-01 19:24:17 +0530

    Initial measseges

---
+++ src/main/java/com/brigdelabz/UserRegistration.java
@@ -0,0 +1,9 @@
+package com.brigdelabz;
+
+import java.util.regex.Pattern;
+
+public class UserRegistration<T> {
+    public static void main(String[] args) {
+        System.out.println("Welcome to junit");
+    }
+}
---
+++ src/test/java/com/brigdelabz/UserRegistrationTest.java
@@ -0,0 +1,8 @@
+package com.brigdelabz;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserRegistrationTest {
+    static UserRegistration userRegistration = new UserRegistration();
+}
